0fac4519edd82b3258f9de4e26f5d18c2fd5a744
Christian Polanco <chrispolanco@sbcglobal.net>
2019-07-07 19:32:55 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+@@count = 0 
+@@genres = []
+@@artists = []
+@@genre_count = {}
+
+  def initialize(name, artist, genre)
+    @name= name 
+    @artist= artist 
+    @genre= genre 
+    @@count += 1 
+    @@genres << genre
+    @@artists << artist 
+  end
+  
+  def self.count
+    @@count 
+  end
+  
+  def self.genres
+    @@genres = @@genres.uniq
+  end
+
+  def self.artists
+    @@artists = @@artists.uniq 
+  end 
+
+end 
